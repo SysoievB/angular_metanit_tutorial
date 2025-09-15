8769967db262dd310d1aa28b6c480602b645ecc1
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-09-15 07:39:09 +0200

    Created and added first component

---
+++ src/first-component/first-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-component',
+  imports: [],
+  templateUrl: './first-component.html',
+  styleUrl: './first-component.css',
+  standalone: true
+})
+export class FirstComponent {
+
+}
