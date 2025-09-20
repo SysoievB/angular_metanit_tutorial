341bf863fcc250d444b93eada9af04e2c215d862
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-09-20 07:43:05 +0200

    Added Template Reference Variables

---
+++ src/app/template-reference-variable/template-reference-variable.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-reference-variable',
+  imports: [],
+  templateUrl: './template-reference-variable.html',
+  styleUrl: './template-reference-variable.css'
+})
+export class TemplateReferenceVariable {
+
+}
