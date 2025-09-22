01b8ef8c7e8b6980b12c17907a4ad03a339fa8ed
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-09-22 05:59:53 +0200

    Added Template Reference Variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,30 @@
-import {Component, signal} from '@angular/core';
+import {Component, ElementRef, signal, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {FirstComponent} from './first-component/first-component';
 import {BindingComponent} from './binding-component/binding-component';
 import {OperatorsComponent} from './operators-component/operators-component';
 import {InputDecoratorComponent} from './input-decorator-component/input-decorator-component';
 import {OutputDecoratorComponent} from './output-decorator-component/output-decorator-component';
+import {TemplateReferenceVariable} from './template-reference-variable/template-reference-variable';
 
 @Component({
   selector: 'app-root',
-  imports: [FirstComponent, BindingComponent, OperatorsComponent, InputDecoratorComponent, OutputDecoratorComponent ],
+  imports: [FirstComponent, BindingComponent, OperatorsComponent, InputDecoratorComponent, OutputDecoratorComponent, TemplateReferenceVariable],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true
 })
 export class App {
+  @ViewChild(TemplateReferenceVariable) templateRefVar!: TemplateReferenceVariable; // DOM element - access to the template reference variable
   name: string = "Tom";
   age: number = 24;
   count = 0;
+
+
+  ngAfterViewInit() {
+    this.templateRefVar.sayHello("World"); // Calls child method
+  }
+
 
   onCountChanged(newCount: number) {
     this.count = newCount;// receives event from child
--- src/app/template-reference-variable/template-reference-variable.ts
+++ src/app/template-reference-variable/template-reference-variable.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-template-reference-variable',
+  selector: 'template-reference-variable',
   imports: [],
   templateUrl: './template-reference-variable.html',
   styleUrl: './template-reference-variable.css'
 })
 export class TemplateReferenceVariable {
 
+  sayHello(name: string) {
+    alert('Hello, ' + name + '!');
+  }
 }
